35c907829c541550ff3bc80c752cffcbab0d7cae
umarhasan <umarf4929@gmail.com>
2024-09-13 23:01:14 +0500

    all project

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if (config('app.env') !== 'production') {
+            DB::listen(function (QueryExecuted $query) {
+                Log::info('sql: ' . $query->sql . ' ' . implode(', ', $query->bindings));
+            });
+        }
+    }
+}
